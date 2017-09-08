c60a56c1e0e3529df82d53881a45ddb8a8d5c209
Oswaldo Almazo <oswaldo.almazo@gmail.com>
2017-09-07 21:03:15 -0400

    add article update and destroy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,8 +6,22 @@ class ArticlesController < ApplicationController
         @articles = Article.all
     end
 
+    def update
+        @article = Article.find(params[:id])
+        if(@article.update(article_params))
+            redirect_to @article
+        else
+            render 'edit'
+        end
+
+    end
+    def edit
+        @article = Article.find(params[ :id ])
+    end
+
     def create
         @article = Article.new(article_params)
+
         if @article.save   
             redirect_to @article 
         else
@@ -17,6 +31,12 @@ class ArticlesController < ApplicationController
 
     def show
         @article = Article.find(params[:id])
+    end
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+
+        redirect_to articles_path
     end
     private 
         def article_params
