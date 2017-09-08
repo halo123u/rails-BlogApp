18a3de48aea9e5b4aef465470173b82669e1c801
Oswaldo Almazo <oswaldo.almazo@gmail.com>
2017-09-07 21:56:13 -0400

    clean up deleting dependents from articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+    http_basic_authenticate_with name: "halo", password: "secret", except: [:index, :show]
+
     def new
         @article  = Article.new
     end
